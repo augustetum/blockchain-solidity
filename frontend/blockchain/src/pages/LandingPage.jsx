250ab7f4bd0dbc26144b83465140338f443fb245
Astrida Jablonskytė <astrida789@gmail.com>
2025-12-17 21:24:06 +0200

    some changes

--- frontend/blockchain/src/pages/LandingPage.jsx
+++ frontend/blockchain/src/pages/LandingPage.jsx
@@ -90,45 +90,6 @@ export default function LandingPage({ navigate }) {
         <div className="features-container">
           <div className="features-grid">
             <div className="features-list">
-              <div className="feature-item">
-                <svg className="feature-icon" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                </svg>
-                <p>The safest way to buy and sell tickets with over 17.6 million fans</p>
-              </div>
-              
-              <div className="feature-item">
-                <svg className="feature-icon" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                </svg>
-                <p>Prices are capped at 20% above face value</p>
-              </div>
-              
-              <div className="feature-item">
-                <svg className="feature-icon" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                </svg>
-                <p>Primary tickets from over 6000 partnered events</p>
-              </div>
-            </div>
-
-            <div className="trustpilot-container">
-              <div className="trustpilot-content">
-                <div className="trustpilot-header">
-                  <svg className="star-icon" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                  </svg>
-                  <span className="trustpilot-text">Trustpilot</span>
-                </div>
-                <div className="stars-container">
-                  {[...Array(5)].map((_, i) => (
-                    <svg key={i} className="star" fill="currentColor" viewBox="0 0 20 20">
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                    </svg>
-                  ))}
-                </div>
-                <p className="reviews-count">26756 reviews</p>
-              </div>
             </div>
           </div>
         </div>
